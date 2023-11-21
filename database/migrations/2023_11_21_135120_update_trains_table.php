b811d428ad8f6a2ab8f913dcb995a7bfa161b7d8
titho93 <titho93.10@gmail.com>
2023-11-21 15:03:08 +0100

    [add]migrate update

---
+++ database/migrations/2023_11_21_135120_update_trains_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->date('arrive_date')->after('train_code');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trains', function (Blueprint $table) {
+            $table->dropColumn('arrive_date');
+        });
+    }
+};
