531d2678dc0a9d44f4ea119a8f22f88b5ec56211
titho93 <titho93.10@gmail.com>
2023-11-22 17:58:14 +0100

    [add]variable train in pagecontroller

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,26 @@
 
 <body>
 
-    <h1>ciao</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+            </tr>
+
+        </tbody>
+    </table>
 
 </body>
 
