b1371bba0159510f3f5efc7b253811a614f1670c
titho93 <titho93.10@gmail.com>
2023-11-22 18:13:36 +0100

    [add]style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,41 +12,46 @@
 
 <body>
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th scope="col">Company</th>
-                <th scope="col">Start_station</th>
-                <th scope="col">Arrival_station</th>
-                <th scope="col">departure_time</th>
-                <th scope="col">arrival_time</th>
-                <th scope="col">train_code</th>
-                <th scope="col">number_carriages</th>
-                <th scope="col">in_time</th>
-                <th scope="col">deleted</th>
-                <th scope="col">arrive_date</th>
-            </tr>
-        </thead>
-        <tbody>
+    <div class="container">
 
-            @foreach ($trains as $train)
+        <h1>Train Stations</h1>
+
+        <table class="table">
+            <thead>
                 <tr>
-                    <td>{{ $train->company }}</td>
-                    <td>{{ $train->start_station }}</td>
-                    <td>{{ $train->arrival_station }}</td>
-                    <td>{{ $train->departure_time }}</td>
-                    <td>{{ $train->arrival_time }}</td>
-                    <td>{{ $train->train_code }}</td>
-                    <td>{{ $train->number_carriages }}</td>
-                    <td>{{ $train->in_time }}</td>
-                    <td>{{ $train->deleted }}</td>
-                    <td>{{ $train->arrive_date }}</td>
-
+                    <th scope="col">Company</th>
+                    <th scope="col">Start_station</th>
+                    <th scope="col">Arrival_station</th>
+                    <th scope="col">departure_time</th>
+                    <th scope="col">arrival_time</th>
+                    <th scope="col">train_code</th>
+                    <th scope="col">number_carriages</th>
+                    <th scope="col">in_time</th>
+                    <th scope="col">deleted</th>
+                    <th scope="col">arrive_date</th>
                 </tr>
-            @endforeach
+            </thead>
+            <tbody>
 
-        </tbody>
-    </table>
+                @foreach ($trains as $train)
+                    <tr>
+                        <td>{{ $train->company }}</td>
+                        <td>{{ $train->start_station }}</td>
+                        <td>{{ $train->arrival_station }}</td>
+                        <td>{{ $train->departure_time }}</td>
+                        <td>{{ $train->arrival_time }}</td>
+                        <td>{{ $train->train_code }}</td>
+                        <td>{{ $train->number_carriages }}</td>
+                        <td>{{ $train->in_time }}</td>
+                        <td>{{ $train->deleted }}</td>
+                        <td>{{ $train->arrive_date }}</td>
+
+                    </tr>
+                @endforeach
+
+            </tbody>
+        </table>
+    </div>
 
 </body>
 
